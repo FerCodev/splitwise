e0abc1955d49e3c1930855c3f27ff023a160726d
Fernando Montes De Oca <codigomontesdeoca@gmail.com>
2026-06-18 19:16:35 -0300

    chore: elimina import y alias de DebugToolbar

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -6,7 +6,6 @@ use App\Filters\AuthFilter;
 use CodeIgniter\Config\Filters as BaseFilters;
 use CodeIgniter\Filters\Cors;
 use CodeIgniter\Filters\CSRF;
-use CodeIgniter\Filters\DebugToolbar;
 use CodeIgniter\Filters\ForceHTTPS;
 use CodeIgniter\Filters\Honeypot;
 use CodeIgniter\Filters\InvalidChars;
@@ -27,7 +26,6 @@ class Filters extends BaseFilters
      */
     public array $aliases = [
         'csrf'          => CSRF::class,
-        // 'toolbar'       => DebugToolbar::class,
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
