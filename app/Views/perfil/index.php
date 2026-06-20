c799c66d4ca8384ce4c8181952280908bc998395
Fernando Montes De Oca <codigomontesdeoca@gmail.com>
2026-06-20 11:49:00 -0300

    chore: limitar fix de wrappers a vistas nuevas

--- app/Views/perfil/index.php
+++ app/Views/perfil/index.php
@@ -1,4 +1,10 @@
-<?= view('partials/_head', ['title' => 'Mi perfil - SplitWise']) ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?= view('partials/_head') ?>
+    <title>Mi perfil - SplitWise</title>
+</head>
+<body>
     <?= view('partials/_navbar', ['pageTitle' => 'Perfil']) ?>
     <div class="container py-4">
         <h2 class="mb-4 d-none d-md-block">Mi perfil</h2>
@@ -47,3 +53,5 @@
         </div>
     </div>
     <?= view('partials/_footer') ?>
+</body>
+</html>
