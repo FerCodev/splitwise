9e3aa8f3033d50982cffa7c96ab02518737e96bc
FerCodev <fernando.proff@hotmail.com>
2026-06-22 14:32:28 -0300

    Prepara entorno local y catalogo de componentes

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -11,6 +11,7 @@ class UserSeeder extends Seeder
         $this->db->table('users')->insert([
             'email' => 'admin@test.com',
             'password' => password_hash('1234', PASSWORD_DEFAULT),
+            'role' => 'admin',
             'name' => 'Admin',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
